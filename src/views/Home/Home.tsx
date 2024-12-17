2419e2631b510338559c4daeaddf9c1a5a3f8b42
pedro-donoso <pedro.donoso.r@usach.cl>
2024-12-17 02:02:58 -0300

    llamado api

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import Header from "../../components/Header";
+import fetchApi from '../../utils/fetch'
 
 const Home = () => {
+  useEffect(() => {
+    const loadTodaysImage = async () => {
+      try {
+        const todaysImage = await fetchApi();
+        console.log(todaysImage)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    loadTodaysImage().catch(null);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Header />

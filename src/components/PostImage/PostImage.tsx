4f60660a48eb02f04e6519631ba13648bff0fb61
pedro-donoso <pedro.donoso.r@usach.cl>
2024-12-22 19:09:20 -0300

    agrego scroll

--- src/components/PostImage/PostImage.tsx
+++ src/components/PostImage/PostImage.tsx
@@ -1,12 +1,23 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { FC } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { PostImage as PostImageTypes } from "../../types";
 
-const PostImage = () => {
+const PostImage: FC<PostImageTypes> = ({ title, date}) => {
   return (
-  <View>
-    <Text>Post image</Text>
-  </View>
+   <View style={styles.container}>
+    <Text>{title}</Text>
+    <Text>{date}</Text>
+    <View>
+      <Button title="View"/>
+    </View>
+   </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'rgba(18,39,113,255)',
+  }
+});
 
 export default PostImage;

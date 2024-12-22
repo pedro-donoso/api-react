dbfb62425896e752ad7fab05af19c6e29e705a2b
pedro-donoso <pedro.donoso.r@usach.cl>
2024-12-22 18:30:47 -0300

    creo componente last five

---
+++ src/components/LastFiveDaysImages/LastFiveDaysImages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const LastFiveDaysImages = () => {
+  return (
+    <View>
+      <Text>Last five days</Text>
+    </View>
+  );
+};
+
+export default LastFiveDaysImages;

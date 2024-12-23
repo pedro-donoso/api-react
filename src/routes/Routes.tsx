15b5cf4cc5fe44d84e0d40e213a94bc2f7f4dd7e
pedro-donoso <pedro.donoso.r@usach.cl>
2024-12-23 02:50:05 -0300

    agrego background

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { RootStackParams } from "../types";
 import Home from "../views/Home";
-import Detail from "../views/Home/Detail";
+import Detail from "../views/Detail";
 
 const Stack = createNativeStackNavigator<RootStackParams>();
 const routeScreenDefaultOptions = {

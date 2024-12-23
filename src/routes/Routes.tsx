a7c1b5c02957d7a41e06226fff0195f37621eebe
pedro-donoso <pedro.donoso.r@usach.cl>
2024-12-23 01:51:03 -0300

    agrego detail

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
 import { RootStackParams } from "../types";
 import Home from "../views/Home";
 import Detail from "../views/Home/Detail";
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParams>();
 
 const Routes = () => (
  <NavigationContainer>
   <Stack.Navigator initialRouteName="Home">
    <Stack.Screen name="Home" component={Home} />
-   <Stack.Screen name="Detail" component={Detail} />
+   <Stack.Screen 
+   name="Detail"
+   component={Detail}
+   options={{ title: 'Detail View' }}
+   />
   </Stack.Navigator>
  </NavigationContainer>
 );

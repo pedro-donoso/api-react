61fa843d943175b4eeee074c1705b21d5240a7a3
pedro-donoso <pedro.donoso.r@usach.cl>
2024-12-25 13:50:04 -0300

    estilo detail

--- src/views/Detail/Detail.tsx
+++ src/views/Detail/Detail.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParams } from "../../types";
 
 const Detail = () => {
+  const {} = useRoute<NativeStackScreenProps<RootStackParams, 'Detail'>['route']>();
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Detail</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(7,26,93,255)'
+  }
+})
 
 export default Detail;

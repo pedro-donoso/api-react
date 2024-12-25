d0db4f57ea1590057d6ef30df89c8ef6d2e5cbbc
pedro-donoso <pedro.donoso.r@usach.cl>
2024-12-25 13:53:46 -0300

    header en detail

--- src/views/Detail/Detail.tsx
+++ src/views/Detail/Detail.tsx
@@ -3,23 +3,27 @@ import { View, Text, StyleSheet } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParams } from "../../types";
+import Header from "../../components/Header";
 
 const Detail = () => {
-  const {} = useRoute<NativeStackScreenProps<RootStackParams, 'Detail'>['route']>();
+ const {
+  params: { title, url, explanation, date },
+ } = useRoute<NativeStackScreenProps<RootStackParams, "Detail">["route"]>();
 
-  return (
-    <View style={styles.container}>
-      <Text>Detail</Text>
-    </View>
-  );
+ return (
+  <View style={styles.container}>
+    <Header />
+   <Text>Detail</Text>
+  </View>
+ );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 16,
-    backgroundColor: 'rgba(7,26,93,255)'
-  }
-})
+ container: {
+  flex: 1,
+  paddingHorizontal: 16,
+  backgroundColor: "rgba(7,26,93,255)",
+ },
+});
 
 export default Detail;
